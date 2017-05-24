c80c0535cf9ff1dd447fd3caac85c2df731211b8
Predrag Cokulov <predragcokulov@gmail.com>
2017-05-24 08:56:22 +0200

    removed fps capping

--- core/src/com/newtecsolutions/mario_gdx_tutorial/GameScreen.java
+++ core/src/com/newtecsolutions/mario_gdx_tutorial/GameScreen.java
@@ -81,8 +81,6 @@ public class GameScreen implements Screen, InputProcessor
     @Override
     public void render(float delta)
     {
-        if(delta > 0.016f)
-            delta = 0.016f;
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
